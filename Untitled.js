50629ede55f96ebe55e68898bdc998a75fc16b67
Noah Rolf <noahrolf1@gmail.com>
2017-06-07 10:06:47 -0500

    added a feature that sends you to a next level

--- Untitled.js
+++ Untitled.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-
+  setDraw();
   var canvas = document.getElementById('myCanvas');
-  //stores the cavas element the game will be built on
+  //stores the canvas element the game will be built on
   var ctx = canvas.getContext('2d');
   //stores the context that the canvas element is 2D so I can paint on it
   var ballRadius = 10;
@@ -20,7 +20,22 @@ $(document).ready(function(){
   //defines where the paddle is located on the canvas
   var rightPressed = false;
   var leftPressed = false;
+  var upPressed = false;
   //placeholder variables defining that the keys at default are not pressed
+  var brickRowCount = 3;
+  var brickColumnCount = 5;
+  var brickWidth = 75;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 30;
+  var brickOffsetLeft = 30;
+  //brick field variables
+  var score = 0;
+  //var for storing score
+
+  var bricks = [];
+    brickLoop();
+  //empty array and two for loops that hold and change the brick cloumns and rows
 
   $(document).on('keydown', keyDownHandler);
   $(document).on('keyup', keyUpHandler);
@@ -29,7 +44,7 @@ $(document).ready(function(){
   function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "rgb(0, 150, 255)";
     ctx.fill();
     ctx.closePath();
   }
@@ -38,22 +53,51 @@ $(document).ready(function(){
   function drawBall() {
       ctx.beginPath();
       ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-      ctx.fillStyle = "#0095DD";
+      ctx.fillStyle = "rgb(0, 200, 255)";
       ctx.fill();
       ctx.closePath();
   }
   //function that draws the ball .arc defines the circle (width, height, radius, start and end angel)
 
+  function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+  }
+  //self explanatory function
+
   function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
-    paddleBoundaries();
+    paddleMovementAndBoundaries();
     collisionDetection();
+    drawScore();
     x += dx;
     y += dy;
   }
-  //resets the canvas and defines ball and paddle location
+  //resets the canvas and calls all functions to draw location and detect collision
+
+  function drawBricks() {
+      for(c=0; c<brickColumnCount; c++) {
+          for(r=0; r<brickRowCount; r++) {
+              if(bricks[c][r].status == 1) {
+              var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+              var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+              //brick dimensions
+              bricks[c][r].x = brickX;
+              bricks[c][r].y = brickY;
+              ctx.beginPath();
+              ctx.rect(brickX, brickY, brickWidth, brickHeight);
+              ctx.fillStyle = "#0095DD";
+              ctx.fill();
+              ctx.closePath();
+              }
+          }
+      }
+  }
+  //draws the bricks by looping through the array defined above
 
   function collisionDetection(){
       if(x + dx > canvas.width - ballRadius  || x + dx < ballRadius) {
@@ -72,8 +116,41 @@ $(document).ready(function(){
              document.location.reload();
              }
       }
-  }
+      for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    console.log(b);
+                    if(score == brickRowCount*brickColumnCount) {
+                      alert('next Level');
+                      ctx.canvas.height += 150;
+                      ctx.canvas.width += 150;
+                      for(c=0; c<brickColumnCount; c++) {
+                        for(r=0; r<brickRowCount; r++) {
+                              console.log(b);
+                              b.status = 1;
+                          }
+                        }
+                      brickRowCount += 1;
+                      brickColumnCount += 2;
+                      brickLoop();
+                      setDraw();
+                      score = 0;
+                      paddleX = (canvas.width-paddleWidth)/2;
+                      paddleY = canvas.height-paddleHeight;
+                      x = canvas.width/2;
+                      y = canvas.height-30;
 
+                    }
+                  }
+              }
+          }
+      }
+  }
   //checks if the ball location is greater or less than any of the canvas borders. If its less than the bottom border,
   //trigger a game reset. If
 
@@ -94,10 +171,10 @@ $(document).ready(function(){
           leftPressed = false;
       }
   }
-//these functions allow movement where keycode 39 of pressing the right arrow sets the var to true,
-//releasing it sets it to false. the same is true for the keycode 37 of the left arrow
+  //these functions allow movement where keycode 39 of pressing the right arrow sets the var to true,
+  //releasing it sets it to false. the same is true for the keycode 37 of the left arrow
 
-  function paddleBoundaries(){
+  function paddleMovementAndBoundaries(){
 
       if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
@@ -106,12 +183,27 @@ $(document).ready(function(){
         paddleX -= 7;
       }
   }
-//this if else statement sets paddle positioning when the vars change due to key press. the second half of the
-//statement indicates that the paddle cannot go beyond the canvas border limits.
+  //this if else statement sets paddle positioning when the vars change due to key press. the second half of the
+  //statement indicates that the paddle cannot go beyond the canvas border limits.
+var intervalID;
+function setDraw(){
+  if (intervalID) {
+    console.log('interval cleared');
+        clearInterval(intervalID);
+    }
+    intervalID = setInterval(draw, 10);
+}
 
-  setInterval(draw, 10);
   //calling setInterval on draw makes it so that the ball will be called every 10ms
   //until we stop it.
 
+  function brickLoop(){
+    for(c=0; c<brickColumnCount; c++) {
+      bricks[c] = [];
+      for(r=0; r<brickRowCount; r++) {
+          bricks[c][r] = { x: 0, y: 0, status: 1 };
+      }
+    }
+  }
 
 });
